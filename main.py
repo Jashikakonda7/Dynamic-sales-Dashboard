c3e3f27e014dc352d59de6415b4c53aa477bf369
Jashikakonda7 <kondajashika@gmail.com>
2026-04-30 06:15:09 +0530

    fix uploads folder

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from database import init_db
 from routers import auth, files, dashboard
+
+os.makedirs("uploads", exist_ok=True)
+
+app = FastAPI(title="Dynamic Sales Dashboard API", version="1.0.0")
 
 app = FastAPI(title="Dynamic Sales Dashboard API", version="1.0.0")
 
